18491739dd362fc33af7e5af9df646292523bb7f
zjl <838976086@qq.com>
2020-09-30 12:24:34 +0800

    commit

--- scrapyspider/spiders/quotes_spider.py
+++ scrapyspider/spiders/quotes_spider.py
@@ -1,9 +1,11 @@
 import scrapy
 import xlrd
+import pymysql
 
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
+    dict = {}
 
     def start_requests(self):
 
@@ -14,15 +16,20 @@ class QuotesSpider(scrapy.Spider):
         self.log("行数 %s" % nrow)
         urls = []
         for i in range(nrow):
+            dict['name'] = table.row(i-1)[0].value
+            dict['url'] = table.row(i-1)[1].value+'/'
             urls.append(table.row(i-1)[1].value+'/')
         for url in urls:
-            self.log("新增url %s" % url)
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
+        db = pymysql.connect("localhost","root","1qaz2wsx","test")
+        sql = "insert into switch(`name`, url, content) value ('%s','%s','%s') " % \
+              ('')
         data1 = response.xpath(
             '//article/div[@class="entry-content"]/div[5]').get()
         data2 = response.xpath(
             '//article/div[@class="entry-content"]/div[6]').get()
         self.log('saved data1 %s' % data1)
         self.log('saved data2 %s' % data2)
+        
